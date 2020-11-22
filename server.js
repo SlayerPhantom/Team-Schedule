c6abce72bf41d2fa9a3c50d263d949844cc1544c
SlayerPhantom <unravelphantom@gmail.com>
2020-11-21 23:51:41 -0500

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({ path: './config.env' });
 const connectDB = require('./db');
 
 connectDB();
