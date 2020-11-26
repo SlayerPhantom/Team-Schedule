02098884e926825c3f5cfac17d6158a55c677f5f
SlayerPhantom <unravelphantom@gmail.com>
2020-11-25 22:30:16 -0500

    commit

--- frontend/src/utils/buildURL.js
+++ frontend/src/utils/buildURL.js
@@ -1,7 +1,7 @@
 const buildURL = (path) => {
 	// if (NODE_ENV === 'production')
-	// return `https://scheduler9.herokuapp.com/${path}`;
-	return `http://localhost:5000/${path}`;
+	return `https://scheduler9.herokuapp.com/${path}`;
+	// return `http://localhost:5000/${path}`;
 };
 
 module.exports = buildURL;
